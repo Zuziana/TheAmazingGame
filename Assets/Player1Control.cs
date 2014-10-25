8f761136d2aec5090d917f1a4d2dc03128da2929
Zuzanna <zuzanna.kurzawa@gmail.com>
2014-10-25 23:55:28 +0200

    The start of the project!

---
+++ Assets/Player1Control.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player1Control : MonoBehaviour {
+	Vector2 newVector;
+	
+	public KeyCode moveUp;
+	
+	public KeyCode moveDown;
+
+	public KeyCode moveRight;
+	
+	public KeyCode moveLeft;
+
+	public float maxSpeed = 10;
+
+	public float acceleration = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		newVector = rigidbody2D.velocity;
+
+		if(Input.GetKey(moveUp)){
+			newVector.y = newVector.y + this.acceleration;
+		}
+		else if(Input.GetKey(moveDown)){
+			newVector.y = newVector.y - this.acceleration;
+		}
+		else if(Input.GetKey(moveLeft)){
+			newVector.x = newVector.x - this.acceleration;
+		}
+		else if(Input.GetKey(moveRight)){
+			newVector.x = newVector.x + this.acceleration;
+		}
+
+		rigidbody2D.velocity = newVector;
+	}
+}

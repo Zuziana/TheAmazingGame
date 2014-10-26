8981a1d8f47c1f6ec94395be4ceea2460724c992
Hector <hjavierog@gmail.com>
2014-10-26 21:24:00 +0100

    Added Bounciness to ball. Added Random start of ball's direction.

---
+++ Assets/TheBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float minRange = 0f;
+		float maxRange = 1f;
+
+		// Randomize 
+
+		if (Mathf.Round(Random.Range (minRange, maxRange)) == 0) {
+			// Left
+			rigidbody2D.AddForce(new Vector2(80, 10));
+		} else {
+			// Right
+			rigidbody2D.AddForce(new Vector2(-80, -10));
+		}
+
+		// Set speed of ball with defined direction
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

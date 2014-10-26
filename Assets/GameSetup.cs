64e7f5aa6b6ca82013228ab3ff0ba97eda0f6f10
Hector <hjavierog@gmail.com>
2014-10-26 19:49:59 +0100

    Put walls on their right places! Now game is AMAZING +

--- Assets/GameSetup.cs
+++ Assets/GameSetup.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 
 public class GameSetup : MonoBehaviour {
-	public BoxCollider2D TopWall;
-	public BoxCollider2D BottomWall;
-	public BoxCollider2D LeftWall;
-	public BoxCollider2D RightWall;
+	public Camera mainCamera;
+
+	public BoxCollider2D topWall;
+	public BoxCollider2D bottomWall;
+	public BoxCollider2D leftWall;
+	public BoxCollider2D rightWall;
 
 	// Update is called once per frame
 	void Update () {
 		// Sizes
-		TopWall.size = new Vector2( 0f, 0f);
+		topWall.size = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f); 
+		bottomWall.size = new Vector2(mainCamera.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f); 
+		rightWall.size = new Vector2(1f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y); 
+		leftWall.size = new Vector2(1f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y); 
 
 		// Locations
-		TopWall.center = new Vector2 (0f, 0f);
+		topWall.center = new Vector2 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + 0.5f);
+		bottomWall.center = new Vector2 (0f, mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - 0.5f);
+		rightWall.center = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+		leftWall.center = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
 	}
 }

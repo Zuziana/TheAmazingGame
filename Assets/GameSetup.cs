000abaff11fb28cc53f9f052c4895cfb99ab9862
Hector <hjavierog@gmail.com>
2014-10-26 19:37:13 +0100

    Added GameSetup: will put all game setup related scripts here. Added wall components: box collider type.

---
+++ Assets/GameSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour {
+	public BoxCollider2D TopWall;
+	public BoxCollider2D BottomWall;
+	public BoxCollider2D LeftWall;
+	public BoxCollider2D RightWall;
+
+	// Update is called once per frame
+	void Update () {
+		// Sizes
+		TopWall.size = new Vector2( 0f, 0f);
+
+		// Locations
+		TopWall.center = new Vector2 (0f, 0f);
+	}
+}
